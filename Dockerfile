d5414bd8eb568cc80bae893940fa4afcabb49c26
Denis Potapov <denis.potapov@esr-nb-1410.esrlabs.local>
2020-01-23 15:44:08 +0100

    generate some docs

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM alpine:3.10
+FROM ruby
+COPY Gemfile /Gemfile
+RUN bundle install
+RUN \
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh ; \
+    sh /tmp/rustup.sh -y ; \
+    . $HOME/.cargo/env ; \
+    cargo install mdbook
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["sh", "/entrypoint.sh"]
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,43 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
-pwd
-touch testfile.from-docker
-ls
+main () {
+    generate_jekyll_site
+    generate_mdbook_content
+    replace_content
+    commit_updated_files
+    ls
+}
 
+generate_jekyll_site () {
+    (
+        cd master_branch
+        bundle exec jekyll build
+    )
+}
+
+generate_mdbook_content () {
+    (
+        . $HOME/.cargo/env
+        cd master_branch/book
+        mdbook build
+    )
+}
+
+replace_content () {
+    test -e pages_branch/build && rm -rf pages_branch/build
+    mv master_branch/_site pages_branch/build
+    test -e pages_branch/build/book && rm -rf pages_branch/build/book
+    mv master_branch/book/book pages_branch/build/book
+}
+
+
+commit_updated_files () {
+    (
+        cd pages_branch
+        git add -A
+        # TODO: commit
+        git status
+    )
+}
+
+main
